365e3051b7fb451bd9e4ce6047f8c99adc4d0334
Chris Ladd <c.g.ladd@gmail.com>
2020-02-06 11:39:05 -0500

    moves parsing to DashDashSwift

--- src/Grate/main.swift
+++ src/Grate/main.swift
@@ -7,52 +7,59 @@
 //
 
 import Foundation
+import DashDashSwift
 
 let DEFAULT_BUFFER_LENGTH = 2048
 
-let inputFlag = Flag(key: "input", shortKey: "i", description: "The location where audio should be loaded from. You may also pass this as the first argument.")
-let outputFlag = Flag(key: "output", shortKey: "o", description: "The location where audio should be saved to. Can be the second argument.")
-let sizeFlag = Flag(key: "size", shortKey: "s", description: "The size of buffers cut from the audio file. Should be a power of 2. Defaults to \(DEFAULT_BUFFER_LENGTH)")
-let labelFlag = Flag(key: "label", shortKey: "l", description: "The expected label to be represented by the data")
-let chromaFlag = Flag(key: "chroma", shortKey: "c", description: "A path to export jpgs of a chromagram from FFT bins")
-
-
-func showHelp() {
-    print("\nGrate helps slice up audio files into test buffers.\n")
-    print(inputFlag.message())
-    print(outputFlag.message())
-    print(labelFlag.message())
-    print(sizeFlag.message())
-    print(chromaFlag.message())
-    print("")
-}
-
-let parser = FlagParser()
 let packager = BufferPackager()
 
-if parser.boolForKey("help", shortKey: "h", args: CommandLine.arguments) {
-    showHelp()
+var parser = CommandLineParser()
+parser.arguments = CommandLine.arguments
+
+parser.register(key: "input",
+                shortKey: "i",
+                description: "The location where audio should be loaded from. You may also pass this as the first argument.")
+
+parser.register(key: "output",
+                shortKey: "o",
+                description: "The location where audio should be saved to. Can be the second argument.")
+
+parser.register(key: "size",
+                shortKey: "s",
+                description: "The size of buffers cut from the audio file. Should be a power of 2. Defaults to \(DEFAULT_BUFFER_LENGTH)")
+
+parser.register(key: "label",
+                shortKey: "l",
+                description: "The expected label to be represented by the data")
+
+parser.register(key: "chroma",
+                shortKey: "c",
+                description: "A path to export jpgs of a chromagram from FFT bins")
+
+
+if parser.boolForKey("help") {
+    print(parser.help());
 }
 
-let chromaPath = parser.dirForFlag(chromaFlag, args: CommandLine.arguments)
+let chromaPath = parser.dirFor(key: "chroma")
 
 var outputIdx = 1
-var input = parser.dirForFlag(inputFlag, args: CommandLine.arguments)
+var input = parser.dirFor(key: "input")
 if input != nil {
     outputIdx = 0
 }
 
-var output = parser.dirForFlag(outputFlag, args: CommandLine.arguments)
+var output = parser.dirFor(key: "output")
 
 let bufferLength: Int
-if let size = parser.intForFlag(sizeFlag, args:CommandLine.arguments) {
+if let size = parser.intFor(key: "size") {
     bufferLength = size
 }
 else {
     bufferLength = DEFAULT_BUFFER_LENGTH
 }
 
-let unflagged = parser.unflaggedArgumentsFrom(CommandLine.arguments)
+let unflagged = parser.unflaggedArguments()
 if input == nil && unflagged.count > 0 {
     input = parser.dirWithPath(unflagged[0])
 }
@@ -61,31 +68,27 @@ if output == nil && unflagged.count > outputIdx {
     output = parser.dirWithPath(unflagged[outputIdx])
 }
 
-//print("Input: \(String(describing: input))")
-//print("Output: \(String(describing: output))")
-//print("Length:   \(bufferLength)")
-
 let logBufferSize = log2(Double(bufferLength))
 guard ceil(logBufferSize) == floor(logBufferSize) else {
     print("Buffer should be a power of two")
-    showHelp()
+    parser.printHelp()
     fatalError()
 }
 
 guard let input = input else {
     print ("\nERROR: Input must be supplied")
-    showHelp()
+    parser.printHelp()
     fatalError()
 }
 
 guard let output = output else {
     print ("\nERROR: Output must be supplied")
-    showHelp()
+    parser.printHelp()
     fatalError()
 }
 
 let label: String;
-if let suppliedLabel = parser.stringForFlag(labelFlag, args: CommandLine.arguments) {
+if let suppliedLabel = parser.stringFor(key: "label") {
     label = suppliedLabel
 }
 else {
